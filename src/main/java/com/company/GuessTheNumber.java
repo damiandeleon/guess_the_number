796ff153a60cbba6e56bbb1d6562befceaf8d01b
Damian Deleon <damianmdeleon@gmail.com>
2022-02-10 14:40:06 -0600

    moved play again question

--- src/main/java/com/company/GuessTheNumber.java
+++ src/main/java/com/company/GuessTheNumber.java
@@ -30,19 +30,20 @@ public class GuessTheNumber {
                     count++;
                 } else if (userGuess == number){
                     System.out.println("Good job, " + userName + "! You guess my number in " + count + " guesses!");
-                     System.out.println("Would you like to play again? (y/n)");
-                     Scanner scanner2 = new Scanner(System.in);
-                     String playAgain = scanner2.nextLine();
-
-                     if(playAgain.equals("y")){
-                         count = 1;
-                     } else {
                          count =+ 7;
-                         userChoice = "n";
-                     }
                 }
             }
-            userChoice = "n";
+            System.out.println("Would you like to play again? (y/n)");
+            Scanner scanner2 = new Scanner(System.in);
+            String playAgain = scanner2.nextLine();
+
+            if(playAgain.equals("y")){
+                count = 1;
+            } else {
+                count =+ 7;
+                userChoice = "n";
+            }
+
         }
     }
 }
